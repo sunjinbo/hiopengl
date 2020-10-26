329d9629feeb82a77454bebdbca72a7cd86a052f
sunjinbo <sunjinbo@360.cn>
2020-10-26 18:06:33 +0800

    实现各种绘制opengl的方式

--- app/src/main/java/com/hiopengl/MainActivity.java
+++ app/src/main/java/com/hiopengl/MainActivity.java
@@ -2,7 +2,9 @@ package com.hiopengl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onP1Click(View view) {
+        startActivity(new Intent(this, GLSurfaceViewActivity.class));
+    }
+
+    public void onP2Click(View view) {
+        startActivity(new Intent(this, TextureViewActivity.class));
+    }
+
+    public void onP3Click(View view) {
+        startActivity(new Intent(this, SurfaceViewActivity.class));
+    }
+
+    public void onP4Click(View view) {
+        startActivity(new Intent(this, CanvasActivity.class));
     }
 }
---
+++ app/src/main/java/com/hiopengl/SurfaceViewActivity.java
@@ -0,0 +1,152 @@
+package com.hiopengl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+import javax.microedition.khronos.opengles.GL10;
+
+public class SurfaceViewActivity extends AppCompatActivity
+        implements SurfaceHolder.Callback, Runnable {
+
+    private SurfaceView mSurfaceView;
+    private Thread mGLThread;
+    private int mWidth, mHeight;
+    private boolean mRunning = false;
+
+    float[] vertexArray = new float[]{
+            -0.8f , -0.4f * 1.732f , 0.0f ,
+            0.8f , -0.4f * 1.732f , 0.0f ,
+            0.0f , 0.4f * 1.732f , 0.0f ,
+    };
+
+    ByteBuffer vbb;
+    FloatBuffer vertex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surfaceview);
+
+        vbb = ByteBuffer.allocateDirect(vertexArray.length*4);
+        vbb.order(ByteOrder.nativeOrder());
+        vertex = vbb.asFloatBuffer();
+        vertex.put(vertexArray);
+        vertex.position(0);
+
+        mSurfaceView = findViewById(R.id.surface_view);
+        mSurfaceView.getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mGLThread = new Thread(this);
+        mGLThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mWidth = width;
+        mHeight = height;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mRunning = false;
+        try {
+            if (mGLThread != null) {
+                mGLThread.join();
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            mGLThread = null;
+        }
+    }
+
+    @Override
+    public void run() {
+        //创建一个EGL实例
+        EGL10 egl = (EGL10) EGLContext.getEGL();
+        //
+        EGLDisplay dpy = egl.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        //初始化EGLDisplay
+        int[] version = new int[2];
+        egl.eglInitialize(dpy, version);
+
+        int[] configSpec = {
+                EGL10.EGL_RED_SIZE,      5,
+                EGL10.EGL_GREEN_SIZE,    6,
+                EGL10.EGL_BLUE_SIZE,     5,
+                EGL10.EGL_DEPTH_SIZE,   16,
+                EGL10.EGL_NONE
+        };
+
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] num_config = new int[1];
+        //选择config创建opengl运行环境
+        egl.eglChooseConfig(dpy, configSpec, configs, 1, num_config);
+        EGLConfig config = configs[0];
+
+        EGLContext context = egl.eglCreateContext(dpy, config,
+                EGL10.EGL_NO_CONTEXT, null);
+        //创建新的surface
+        EGLSurface surface = egl.eglCreateWindowSurface(dpy, config, mSurfaceView.getHolder(), null);
+        //将opengles环境设置为当前
+        egl.eglMakeCurrent(dpy, surface, surface, context);
+        //获取当前opengles画布
+        GL10 gl = (GL10)context.getGL();
+
+        init(gl);
+
+        mRunning = true;
+        while (mRunning) {
+            int w, h;
+            synchronized(this) {
+                w = mWidth;
+                h = mHeight;
+            }
+            try {
+                Thread.sleep(333);
+            }
+            catch (InterruptedException ex) {}
+            drawFrame(gl, w, h);
+
+            //显示绘制结果到屏幕上
+            egl.eglSwapBuffers(dpy, surface);
+        }
+        egl.eglMakeCurrent(dpy, EGL10.EGL_NO_SURFACE, EGL10.EGL_NO_SURFACE, EGL10.EGL_NO_CONTEXT);
+        egl.eglDestroySurface(dpy, surface);
+        egl.eglDestroyContext(dpy, context);
+        egl.eglTerminate(dpy);
+    }
+
+    private void init(GL10 gl) {
+        gl.glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
+        // Clears the screen and depth buffer.
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT
+                | GL10.GL_DEPTH_BUFFER_BIT);
+    }
+
+    private void drawFrame(GL10 gl, int w, int h) {
+        gl.glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+        gl.glPointSize(18f);
+        gl.glLoadIdentity();
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertex);
+        gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 3);
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+    }
+}
