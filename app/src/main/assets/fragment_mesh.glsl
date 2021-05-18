364819674a975e9c84b5872e5a51e900fd716210
sunjinbo <sunjinbo@360.cn>
2021-05-18 16:29:34 +0800

    改进mesh显示效果

--- app/src/main/assets/fragment_mesh.glsl
+++ app/src/main/assets/fragment_mesh.glsl
@@ -1,17 +1,18 @@
 #version 300 es
+#extension GL_OES_standard_derivatives : enable
 precision mediump float;
 
 in vec3 vBC;
 
 out vec4 fragColor;
 
+float edgeFactor(){
+    vec3 d = fwidth(vBC);
+    vec3 a3 = smoothstep(vec3(0.0), d * 1.5, vBC);
+    return min(min(a3.x, a3.y), a3.z);
+}
+
 void main() {
-    if (any(lessThan(vBC, vec3(0.01))))
-    {
-        fragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    }
-    else
-    {
-        fragColor = vec4(0.0, 0.0, 0.0, 0.0);
-    }
+    fragColor.rgb = mix(vec3(0.0), vec3(1.0), edgeFactor());
+    fragColor.a = 1.0;
 }
